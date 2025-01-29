91fc2779ae4fe04ca83e0cfc4f8e7e68c419494f
Edgaras Laptevas <edgaraslaptevas88gmail.com>
2025-01-29 13:11:33 +0200

    Finished 2.1 task

---
+++ getHello/src/test/java/techin/lt/getHello/GetHelloApplicationTests.java
@@ -0,0 +1,13 @@
+package techin.lt.getHello;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GetHelloApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
